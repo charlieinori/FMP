3d1b660db9e56fed906462ef5a17682955ab4e4a
ESCG\530008 <530008@chrome.escg.ac.uk>
2023-04-26 16:16:21 +0100

    movement basic
    fr

--- Assets/BirdMovment.cs
+++ Assets/BirdMovment.cs
@@ -1,30 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using TMPro;
 public class BirdMovment : MonoBehaviour
 {
     private CharacterController Controller;
     private Vector3 Velocity;
     private bool Cooldown;
 
-    void Start()
+    private GameObject LookAt;
+    private int speed;
+    public  GameObject Cube1;
+    public GameObject Cube2;
+
+
+    private void Start()
     {
         Controller = gameObject.GetComponent<CharacterController>();
     }
 
-    void Update()
+   private void Update()
     {
         #region Movement 
         Velocity.y += -15 * Time.deltaTime;
-        if (Input.GetKey(" space ") && Cooldown == false)
+        if (Input.GetKey("space") && Cooldown == false)
         {
             Cooldown= true;
             Velocity.y = Mathf.Sqrt(60);
             StartCoroutine(CooldownRefresh());
         }
-        #endregion
 
+        Controller.Move(Velocity * Time.deltaTime);
+        #endregion
+        #region Tilting 
+        if (Velocity.y > 0)
+        {
+            LookAt = Cube1;
+            speed = 5; 
+        }
+        else
+        {
+            LookAt= Cube2;
+            speed = 10; 
+             
+        }
+        
+
+        Quaternion LookOnLook = Quaternion.LookRotation(-LookAt.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, LookOnLook, speed * Time.deltaTime);
+        #endregion
     }
 
     private IEnumerator CooldownRefresh()
